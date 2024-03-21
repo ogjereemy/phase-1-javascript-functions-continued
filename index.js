de3d5df3f5baab3ad3e10dc8c52c858d6dfc50cd
ogola <ogolajeremy601@gmail.com>
2024-03-21 13:42:24 +0300

    adds-func-cont

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(act = "roller-skate"){
+    return `This Saturday, I want to ${act}!`;
+    
+}
+function mondayWork(act2 = "go to the office"){
+    return `This Monday, I will ${act2}.`
+
+}
+function wrapAdjective(rool = "*"){
+    return function(adjective){
+        return `You are ${rool}${adjective}${rool}!`
+    }
+
+}
+    
